fe8e9629fce7882ea14b116f6a0bc3d1ed71926c
GermanSuelaCorrales <62803781+GermanSuelaCorrales@users.noreply.github.com>
2023-10-01 01:12:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,8 +25,8 @@ fi
 # PRE
 ################################################################################
 
-sudo apk add git unzip python2 python2-dev libffi-dev make gcc g++ \
-ncurses-dev avrdude gcc-avr binutils-avr avr-libc \
+sudo apk add git unzip libffi-dev make gcc g++ \
+ncurses-dev avrdude \
 python3 py3-virtualenv \
 python3-dev freetype-dev fribidi-dev harfbuzz-dev jpeg-dev lcms2-dev openjpeg-dev tcl-dev tiff-dev tk-dev zlib-dev \
 jq udev
